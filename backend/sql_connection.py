b4f677261b15207eb687ed4b5795bcc5fb90298c
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-06-30 23:46:12 +0530

    backend/sql_connection.py added

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -5,7 +5,7 @@ __cnx = None
 def get_sql_connection():
     global __cnx
     if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='Swapnil$25',
+        __cnx = mysql.connector.connect(user='root', password='',
                                 host='127.0.0.1',
                                 database='Electric Store')
 
